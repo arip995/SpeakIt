35efea3e8e85394524230de33573bc20e0565210
Arindam Panda <arindampanda2002@gmail.com>
2021-01-13 11:25:00 +0530

    initialcommit

---
+++ eventPage.js
@@ -0,0 +1,14 @@
+var menuItem ={
+    "id":"speak",
+    "title":"Speak",
+    "contexts":["selection"]
+};
+chrome.contextMenus.create(menuItem);
+
+
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+    if (clickData.menuItemId=="speak"&&clickData.selectionText) {
+        chrome.tts.speak(clickData.selectionText,{'rate':0.7});
+    }
+})
